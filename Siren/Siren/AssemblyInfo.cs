03a7e1056160587bcfe0babe9a4845e7f6777365
LokiZzZ <lokizzzzzzzz@gmail.com>
2022-07-02 17:51:28 +0300

    Splash!

--- Siren/Siren/AssemblyInfo.cs
+++ Siren/Siren/AssemblyInfo.cs
@@ -2,4 +2,6 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "FA")] 
+[assembly: ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "FA")]
+[assembly: ExportFont("LibreBaskerville-Regular.ttf", Alias = "Baskerville")] 
+[assembly: ExportFont("SourceSansPro-Regular.ttf", Alias = "SansPro")] 
