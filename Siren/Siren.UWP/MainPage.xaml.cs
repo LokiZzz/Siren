cab5fc5651636b43c6458c9dc1ec12385ca4f061
LokiZzZ <lokizzzzzzzz@gmail.com>
2022-07-12 17:43:53 +0300

    Maximize after second launch;

--- Siren/Siren.UWP/MainPage.xaml.cs
+++ Siren/Siren.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -27,6 +28,23 @@ namespace Siren.UWP
             LoadApplication(new Siren.App());
             Size size = new Size(500, 500);
             ApplicationView.GetForCurrentView().SetPreferredMinSize(size);
+            MaximizeWindowOnLoad();
+        }
+
+        void MaximizeWindowOnLoad()
+        {
+            DisplayInformation view = DisplayInformation.GetForCurrentView();
+
+            // Get the screen resolution (APIs available from 14393 onward).
+            Size resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+
+            // Calculate the screen size in effective pixels. 
+            // Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
+            double scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
+            Size bounds = new Size(resolution.Width / scale, resolution.Height / scale);
+
+            ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
     }
 }
